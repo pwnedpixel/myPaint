ca244c122058ed9e60d321046de53e345719a8be
Andrew <Andy.keech@live.ca>
2017-03-14 16:12:04 -0400

    Added: Line, free hand, rectangle
    they draw over each other, and cause white gaps

--- src/myPaint.java
+++ src/myPaint.java
@@ -1,8 +1,7 @@
 import javax.swing.*;
 import java.applet.Applet;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /**
  * Created by andyk on 2017-03-14.
@@ -12,6 +11,7 @@ public class myPaint extends JFrame {
     JPanel controls;
     String mode="none";
     JLabel modeLbl;
+    int prevx,prevy,startX,startY = 0;
 
     public myPaint(){
         GridBagConstraints c = new GridBagConstraints();
@@ -28,6 +28,7 @@ public class myPaint extends JFrame {
         controls.setPreferredSize(new Dimension(200,360));
         draw.setBorder(BorderFactory.createLineBorder(Color.black));
         controls.setBorder(BorderFactory.createLineBorder(Color.red));
+        draw.setBackground(Color.white);
         this.add(draw,c);
         this.add(controls,c);
 
@@ -36,16 +37,52 @@ public class myPaint extends JFrame {
         controls.add(modeLbl);
         //create and add buttons
         JButton lineBtn = new JButton("line");
-        JButton squareBtn = new JButton("square");
+        JButton squareBtn = new JButton("rectangle");
+        JButton ellipseBtn = new JButton("ellipse");
+        JButton drawBtn = new JButton("draw");
         controls.add(lineBtn);
         controls.add(squareBtn);
+        controls.add(ellipseBtn);
+        controls.add(drawBtn);
 
         //add listeners
         lineBtn.addActionListener(new modeListener());
         squareBtn.addActionListener(new modeListener());
+        ellipseBtn.addActionListener(new modeListener());
+        drawBtn.addActionListener(new modeListener());
 
+        draw.addMouseListener(new mouseButtonListener());
+        draw.addMouseMotionListener(new mouseMoveListener());
     }
 
+    public void drawNewLine(int stx, int sty,int newX, int newY, boolean erase){
+        Graphics g = draw.getGraphics();
+        if (erase) {
+            g.setColor(Color.white);
+            g.drawLine(stx, sty, prevx, prevy);
+        }
+        g.setColor(Color.black);
+        g.drawLine(stx,sty,newX,newY);
+        draw.paintComponents(g);
+    }
+
+    public void drawNewRect(int newX, int newY){
+        int tw,th,tx,ty = 0;
+        Graphics g = draw.getGraphics();
+        g.setColor(Color.white);
+        tw = Math.abs(prevx-startX);
+        th = Math.abs(prevy-startY);
+        tx = Math.min(startX,prevx);
+        ty = Math.min(startY,prevy);
+        g.drawRect(tx,ty,tw,th);
+        g.setColor(Color.black);
+        tw = Math.abs(newX-startX);
+        th = Math.abs(newY-startY);
+        tx = Math.min(startX,newX);
+        ty = Math.min(startY,newY);
+        g.drawRect(tx,ty,tw,th);
+        draw.paintComponents(g);
+    }
 
     public static void main(String[] args){
         myPaint paint = new myPaint();
@@ -62,12 +99,95 @@ public class myPaint extends JFrame {
             modeLbl.setText("Mode: "+mode);
             switch(mode){
                 case "line":
+                    //drawLine();
                     break;
-                case "square":
+                case "rectangle":
+                    break;
+                case "ellipse":
                     break;
                 default:
                     break;
             }
+        }
+    }
+
+    public class mouseButtonListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+            switch(mode){
+                case "line":
+                    startX = e.getX();
+                    startY = e.getY();
+                    prevx = e.getX();
+                    prevy = e.getY();
+                    break;
+                case "rectangle":
+                    startX = e.getX();
+                    startY = e.getY();
+                    prevx = e.getX();
+                    prevy = e.getY();
+                    break;
+                case "ellipse":
+                    break;
+                case "draw":
+                    startX = e.getX();
+                    startY = e.getY();
+                    prevx = e.getX();
+                    prevy = e.getY();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
+
+    public class mouseMoveListener implements MouseMotionListener {
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            switch(mode){
+                case "line":
+                    drawNewLine(startX,startY,e.getX(), e.getY(),true);
+                    break;
+                case "rectangle":
+                    drawNewRect(e.getX(),e.getY());
+                    break;
+                case "ellipse":
+                    break;
+                case "draw":
+                    drawNewLine(prevx,prevy,e.getX(), e.getY(),false);
+                    break;
+                default:
+                    break;
+            }
+            prevx = e.getX();
+            prevy = e.getY();
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            //System.out.println("Move");
         }
     }
 }
